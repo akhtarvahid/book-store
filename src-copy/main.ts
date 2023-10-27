9ba790c29b1859d93c6d0de21212e74d6b83a0cd
Vahid Akhtar <a845260@yara.com>
2023-10-27 21:34:23 +0530

    book store crud

--- src-copy/main.ts
+++ src-copy/main.ts
@@ -1,11 +1,8 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  Logger.log('Application is listening on port:7077');
-  await app.listen(9909);
+  await app.listen(3000);
 }
 bootstrap();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookEntity } from './book/book.entity';
+import { BookModule } from './book/book.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 7080,
+      username: 'postgres',
+      password: 'password',
+      database: 'postgres',
+      synchronize: true,
+      entities: [BookEntity],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: 'src/book-schema.gql',
+      playground: true,
+      driver: ApolloDriver,
+    }),
+    BookModule,
+  ],
 })
 export class AppModule {}
